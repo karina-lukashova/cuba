5710fb62f59d89afd759d0c7aea30662b1ae4c06
Karina Lukashova <karina-luka@yandex.ru>
2020-10-25 17:25:55 +0300

    Сделала блок photo и слайдер

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 $(function(){
-
+  $('.photo__slider').slick({
+    prevArrow: '<button class="slider-arrow slider-prev" type="button"><img src="images/slider-arrow-left.svg"></button>',
+    nextArrow: '<button class="slider-arrow slider-next" type="button"><img src="images/slider-arrow-right.svg"></button>'
+  });
 });
